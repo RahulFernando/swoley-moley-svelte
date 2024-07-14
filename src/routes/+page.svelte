3696d1ec20704453e749ca30500a1f07021a6072
RahulFernando <rahulrulz680@gmail.com>
2024-07-14 13:11:22 +0530

    Initialize

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Hero from "../components/hero.svelte";
+  import Product from "../components/product.svelte";
+  import Reviews from "../components/reviews.svelte";
+</script>
+
+<main class="flex flex-col">
+  <Hero />
+  <Product />
+  <Reviews />
+</main>
